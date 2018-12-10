2514231a745bf4a7b24179fc41d4263ada95d699
Jacob Mazur <JacobFMazur@gmail.com>
2018-12-10 13:30:57 -0600

    Update GameManager.cs

--- Team4 Tower Escape/Assets/Scripts/GameManager.cs
+++ Team4 Tower Escape/Assets/Scripts/GameManager.cs
@@ -1,16 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
     float timeInScene;
     int trigger;
     public GameObject conversationPanel;
 
-	// Use this for initialization
-	void Start () {
+    Text playerName;
+    Text otherName;
+    Text playerWords;
+    Text otherWords;
+    // Use this for initialization
+    void Start () {
         timeInScene = 0f;
         trigger = 0;
+        playerName = GetComponent<Text>();
+        playerName.text = "";
+        otherName = GetComponent<Text>();
+        otherName.text = "";
+        playerWords = GetComponent<Text>();
+        playerWords.text = "";
+        otherWords = GetComponent<Text>();
+        otherWords.text = "";
         foreach (GameObject go in Resources.FindObjectsOfTypeAll(typeof(GameObject)) as GameObject[])
         {
             if (go.CompareTag("ConversationPanel"))
