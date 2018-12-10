6534051994091de3f5f2eafcc699ca21c0bd26c1
Jacob Mazur <JacobFMazur@gmail.com>
2018-12-10 13:27:01 -0600

    Added basic conversation box

--- Team4 Tower Escape/Assets/Scripts/GameManager.cs
+++ Team4 Tower Escape/Assets/Scripts/GameManager.cs
@@ -3,14 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
+    float timeInScene;
+    int trigger;
+    public GameObject conversationPanel;
 
 	// Use this for initialization
 	void Start () {
-		
+        timeInScene = 0f;
+        trigger = 0;
+        foreach (GameObject go in Resources.FindObjectsOfTypeAll(typeof(GameObject)) as GameObject[])
+        {
+            if (go.CompareTag("ConversationPanel"))
+            {
+                conversationPanel = go;
+            }
+        }
+        conversationPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        timeInScene += Time.deltaTime;
+        if (trigger == 0 && timeInScene > 5)
+            firstConversation();
+    
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    void firstConversation()
+    {
+        conversationPanel.SetActive(true);
+    }
 }
